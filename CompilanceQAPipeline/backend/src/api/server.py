613d504afe5517c9f9f03791bfadac4e506cd7bd
Ruchita Rao <raoruchita0@gmail.com>
2026-04-09 18:04:45 +0530

    upadted the server file (api file is been created and needs to be upadted)

---
+++ CompilanceQAPipeline/backend/src/api/server.py
@@ -0,0 +1,4 @@
+"""
+ This is the main api where the users will communicate with the backend. It will handle the incoming requests and route them to the appropriate services.
+ It will also handle the authentication and authorization of the users.
+"""
